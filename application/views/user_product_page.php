e70db6e257b9743f41283177f598d1d5a2f003b5
Shawnte Arrington <shawnte.arrington@gmail.com>
2015-11-25 13:46:44 -0800

    updates to product page

--- application/views/user_product_page.php
+++ application/views/user_product_page.php
@@ -18,34 +18,47 @@
     </head>
     <body>
         <div class = "container">    
-        <header>
+        <div class="page-header">
             <h2>dojo_Mojo</h2>
-            <a id = "shopping_cart" href="">Shopping Cart</a>
-        </header>
             <a id = "store_link" href="">Back to dojo_Mojo Store</a>
+            <button type="button" class="shopping_cart btn-lg" href = " " aria-label="Shopping Cart">
+                <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+            </button>
+            <form id = "logout" action = "/user/logout" method = "POST">
+                <input type = "submit" value = "Logout">
+            </form>
+        </div>
             <h1><?php echo "Product Name" ?></h1>
             <div id = "product_image">
                 <div class = "row">
-                <div class = "col-md-8">
-                <img src = "/assets/weapons_icons/large/1.png">
-                </div>
-                <div class = "col-md-4">
-                <div id = "product_desc">
-                 <p>Ontgonnen perzische in herhaling nu honderden belasting. Bevaarbaar schipbreuk kilometers af al uitgevoerd. In zand alle daad na doet gold waar. Stam twee aan koel zijn aard met geld. Op geslaagd nu trouwens omgeving na omwonden de wakkeren. Oven acre sago in de tijd op wijk. Lot drong noemt aan dal ijzer wezen oog holte stuit. Verwijderd spoorwegen was beschikken bak natuurlijk van mislukking.</p>
-                </div>
-            </div> 
-            <label>Quantity</label>
-            <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
-            <form action = "orders/order/buy" method = "POST">
-                <input type = "submit" value = "Buy!">
-            </form>
-        </div>    
+                    <div class = "col-md-2"> 
+                    </div>   
+                    <div class = "col-md-6">
+                        <img src = "/assets/weapons_icons/large/1.png">
+                    </div>
+                    <div class = "col-md-4">
+                        <div id = "product_desc">
+                            <h3>Product Description</h3>
+                            <p>Ontgonnen perzische in herhaling nu honderden belasting. Bevaarbaar schipbreuk kilometers af al uitgevoerd. In zand alle daad na doet gold waar. Stam twee aan koel zijn aard met geld. Op geslaagd nu trouwens omgeving na omwonden de wakkeren. Oven acre sago in de tijd op wijk.</p>
+                        <div></p>
+                        </div>
+                            <h3>Specs</h3>
+                            <p>STRENGTH ==</p>
+                        </div>
+                        <label>Quantity</label>
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                        <form action = "orders/order/buy" method = "POST">
+                            <input type = "submit" value = "Buy">
+                        </form>
+                    </div> 
+                </div> 
+            </div>       
     </body>
 </html>
 <!--  <img id = "<?php echo $i ?>" src = "assets/product_images/<?php echo $i ?>.png" width = "500px" height = "500px" alt = "product image"> -->
