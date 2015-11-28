ace0235f9192772336714ef728bdf68a8a5d42f8
Shawnte Arrington <shawnte.arrington@gmail.com>
2015-11-28 15:04:14 -0800

    orders_page and partials styling

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -1,3 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Products extends CI_Controller {
+
+	public function search(){
+		FETCH * FROM products WHERE...;
+	}
+}
